38982ba7e00f4c7f0e4ea25c6a82c1404952e1ce
imann24 <contact@isaiahmann.com>
2016-04-12 15:11:22 -0400

    Implementing reading in input from command line in python

--- stable.py
+++ stable.py
@@ -1,86 +1,71 @@
-# import java.util.Scanner;
-#
-# class Stable {
-#
-#   public static void main (String[] args) {
-#       Scanner in = new Scanner(System.in);
-#
-#       // Create Input variables to hold number of men and women:
-#       int choices =0, num=0, n =1, i, j;
-#       int [][] men = null, women = null;
-#       // Print a nice intro the user user
-#
-#       System.out.println("~~~~Welcome to the Marriage Algorithm Program~~~~");
-#       System.out.println("The rules for this program are as follows:");
-#       System.out.println("1. Enter the choices for each man/woman");
-#       System.out.println("   The choices must be enterred with a space between each value");
-#       System.out.println("2. Do not enter a choice that is not on your list");
-#       System.out.println("   This program does not check for such errors");
-#       System.out.println("3. Do not repeat a choice for the same man/woman");
-#       System.out.println("4. Complete each Rank ordered List (ROL)\n\n");
-#
-#        if(args.length == 0) {
-#           // Not sure what this five thing is about, probably worth looking into
-#           // Read in the number of men/women
-#           System.out.println("How many men/women ?");
-#           num = in.nextInt();
-#           while (num > 5) {
-#              System.out.println("Please don't enter a number greater than 5");
-#              System.out.println("Re-enter number:");
-#              num = in.nextInt();
-#           }
-#
-#
-#           // Create two jagged arrays, one for men, one for women
-#           men = new int[num][num+1];
-#           women = new int[num][num+1];
-#
-#
-#           //Read choices for men
-#           System.out.println("Enter your choices for: ");
-#           for (i = 0; i < num; i++) {
-#
-#               System.out.println("Man " + i + ": ");
-#
-#
-#               for (j = 0; j < num; j++) {
-#                   choices = in.nextInt();
-#
-#                   while (choices > num || choices < 0) {
-#                     System.out.println("Please enter a valid choice for Man " + i + " between 0 and " + (num-1) + ": ");
-#                     choices = in.nextInt();
-#                   }
-#
-#                   men[i][j+1] = choices;
-#               }
-#
-#             }
-#
-#             System.out.println("\n");
-#
-#
-#
-#             // Read choices for women
-#             for (i = 0; i < num; i++) {
-#                 System.out.println("Woman " + i + ": ");
-#
-#                 for (j = 0; j < num; j++) {
-#                     choices = in.nextInt();
-#
-#                     while (choices > num || choices < 0) {
-#                       System.out.println("Please enter a valid choice for Woman " + i + " between 0 and " + (num-1) + ": ");
-#                       choices = in.nextInt();
-#                     }
-#                     women[i][j+1] = choices;
-#                 }
-#             }
-#           } else {
-#
-#               System.out.println("write the method to read input from a file");
-#               System.exit(0);
-#
-#           }
-#
+# Authors: Isaiah Mann, Kaylynn Crawford, Zoe Kendall
+# Description: Python Implementation of Stable Marriage problem
+# Adapted from: C++ code by Samia Qader
+
+import re
+import sys
+
+# UTILITY METHODS:
+def create_2d_int_array (x, y):
+    return [[0 for i in range(y)] for j in range(x)]
+
+def next_int (message = None):
+    if (message == None):
+        return int(input())
+    else:
+        return int(input(message))
+
+# Reading in Input:
+choices = 0
+num = 0
+n = 0
+i = 0
+j = 0
+
+print("~~~~Welcome to the Marriage Algorithm Program~~~~")
+print("The rules for this program are as follows:")
+print("1. Enter the choices for each man/woman")
+print("   The choices must be enterred with a space between each value")
+print("2. Do not enter a choice that is not on your list")
+print("   This program does not check for such errors")
+print("3. Do not repeat a choice for the same man/woman")
+print("4. Complete each Rank ordered List (ROL)\n\n")
+
+# If there is no file passed to the program
+if(len(sys.argv) <= 1):
+
+    num = next_int("How many men/women ?\n")
+    while (num > 5):
+        print("Please don't enter a number greater than 5")
+        num = next_int("Re-enter number:\n")
+
+    men = create_2d_int_array(num, num+1)
+    women = create_2d_int_array(num, num+1)
+
+    for i in range (0, num):
+        print ("Man " + str(i) + ": ")
+
+        for j in range (0, num):
+            choices = next_int()
+
+            while (choices > num or choices < 0):
+                print("Please enter a valid choice for Man " + str(i) + " between 0 and " + str(num-1) + ": ")
+                choices = next_int()
+            men[i][j+1] = choices
+
+    for i in range (0, num):
+        print ("Woman " + str(i) + ": ")
+
+        for j in range (0, num):
+            choices = next_int()
+
+            while (choices > num or choices < 0):
+                print("Please enter a valid choice for Woman " + str(i) + " between 0 and " + str(num-1) + ": ")
+                choices = next_int()
+            women[i][j+1] = choices
+else:
+    print ("Not Yet implemented: Reading input from a file.\nExiting...")
+    sys.exit(0)
 #       // Actual run the stable matching algorithm:
 #       //Marriage Algorithm
 #       int man= -1, woman= -1, ugly_man = num + 1, temp;
@@ -153,5 +138,3 @@
 # }
 
 # Code adapted from http://stackoverflow.com/questions/2397141/how-to-initialize-a-two-dimensional-array-in-python
-def create_2d_int_array (x, y):
-    return [[0 for i in range(x)] for j in range(y)]
